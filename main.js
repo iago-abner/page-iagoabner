b5e7fbe7482b92fe78d7c0062404c67cfd7c1656
iago_abner <iagoabner.dev@gmail.com>
2022-02-25 20:58:39 -0300

    changes css

---
+++ main.js
@@ -0,0 +1,7 @@
+function displayAbout(){
+  let arrow = document.getElementById('animation');
+  arrow.classList.toggle('rotate');
+  let display = document.getElementById('about');
+  display.classList.toggle('slide-bottom')
+}
+
